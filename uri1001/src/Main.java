d8bfe76d405e07dc9f10ab1b7becdfb90b1c88f4
Leonardo Malikoski <uleonardomalikoski@gmail.com>
2024-07-15 00:13:17 -0300

    first commit

---
+++ uri1001/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+        int X = A + B;
+
+        System.out.printf("X = %d%n", X);
+
+    }
+}
