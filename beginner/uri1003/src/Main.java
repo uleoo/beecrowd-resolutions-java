9a03fd23110bcdb1756d717012f384934ffdb73e
Leonardo Malikoski <uleonardomalikoski@gmail.com>
2024-07-15 12:33:28 -0300

    newResolution

---
+++ beginner/uri1003/src/Main.java
@@ -0,0 +1,18 @@
+package beginner.uri1003.src;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+
+        int soma = A + B;
+
+        System.out.printf("SOMA = %d%n", soma);
+
+    }
+}
