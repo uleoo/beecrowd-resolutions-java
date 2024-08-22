0ca1391ccdc0eea828a1c1b7e33619f44cec1891
Leonardo Malikoski <uleonardomalikoski@gmail.com>
2024-08-22 19:55:43 -0300

    newResolutions

---
+++ beginner/uri1059/src/Main.java
@@ -0,0 +1,12 @@
+package beginner.uri1059.src;
+
+public class Main {
+    public static void main (String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
